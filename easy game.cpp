b7e8b6cc65864962f5d9e49f20d5228c943cd761
Blaze0018 <73944871+Blaze0018@users.noreply.github.com>
2020-11-05 11:21:56 +0800

    Add files via upload

--- easy game.cpp
+++ easy game.cpp
@@ -2,22 +2,22 @@
 int main(){
 	printf("Test 1.00\n");
 	int hp = 100,atk = 30,def = 10,choice,round=0;
-	printf("小怪A出现了!\n");
+	printf("Enemy A\n");
 	int enemyhp = 100,enemyatk = 15,enemydef = 10;
 	while(enemyhp>=0){
 		printf("round %d\n",++round);
-		printf("敌方HP=%d\n你的HP=%d\n",enemyhp,hp);
+		printf("Its HP=%d\nYour HP=%d\n",enemyhp,hp);
 		printf("choose\n1.attack 2.defence\n");
 		scanf("%d",&choice);
 		if(choice == 1){
-			printf("你攻击了！\n");
+			printf("You attacked！\n");
 			enemyhp = enemyhp - (atk - enemydef);
 			if(enemyhp <= 0){
 				break;
 			}
 		}
 		else if(choice = 2){
-			printf("你进行了防御！\n");
+			printf("You defended！\n");
 			def = def + 5;
 		}
 			else{
@@ -29,11 +29,12 @@ int main(){
 		if(choice = 2){
 			def = def - 5;
 		}
-		printf("敌人仍没有倒下\n");
+		printf("Enemy is still alive!\n");
 		if(hp <= 0){
 			printf("you died!\n");
 			break;
 		}
+	fflush(stdin);
 	}
 	if(hp > 0){
 		printf("you win!\n");
